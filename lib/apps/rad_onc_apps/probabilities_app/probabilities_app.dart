b9788ae06121d2d44e8e831913362ced0c4c1774
JP Obeid <jpobeid91@gmail.com>
2021-08-10 22:14:11 -0700

    Added dropdown bar

--- lib/apps/rad_onc_apps/probabilities_app/probabilities_app.dart
+++ lib/apps/rad_onc_apps/probabilities_app/probabilities_app.dart
@@ -5,11 +5,15 @@ import 'package:rad_onc_project/data/global_data.dart' as datas;
 class ProbabilitiesApp extends StatefulWidget {
   static const String routeName = '/probabilities-app';
 
+  static const List<String> probTypes = ['Fixed rate', 'Exp. Survival'];
+
   @override
   _ProbabilitiesAppState createState() => _ProbabilitiesAppState();
 }
 
 class _ProbabilitiesAppState extends State<ProbabilitiesApp> {
+  int _iType = 0;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -17,8 +21,29 @@ class _ProbabilitiesAppState extends State<ProbabilitiesApp> {
         appBar: RadAppBar(
           strAppTitle: datas.mapAppNames[0]![2],
         ),
-        body: Container(
-          color: Colors.blue,
+        body: Column(
+          children: [
+            DropdownButton(
+              dropdownColor: Colors.blueGrey,
+              value: _iType,
+              items: ProbabilitiesApp.probTypes
+                  .map(
+                    (e) => DropdownMenuItem(
+                      child: Text(
+                        e,
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                      value: ProbabilitiesApp.probTypes.indexOf(e),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (int? index) {
+                setState(() {
+                  _iType = index!;
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
