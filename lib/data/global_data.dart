3c935e9b039640a9c0effe1b305b46313582b082
JP Obeid <jpobeid91@gmail.com>
2021-08-23 12:43:21 -0700

    Build version 1.0.0 ready

--- lib/data/global_data.dart
+++ lib/data/global_data.dart
@@ -1,4 +1,4 @@
-const String appName = 'Rad-Onc Tools';
+const String appName = 'RadOnc Tools';
 const String appAuthor = 'Jean-Pierre Obeid';
 const String appInstitution = 'Stanford University';
 const String appYear = '2021';
